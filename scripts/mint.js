7abf1853ee52e98987dc2cd8dcc7ec5a6d2f8e9c
Maidyo3030 <h_necat@hotmail.com>
2024-08-14 13:00:26 +0000

    Initial commit

---
+++ scripts/mint.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x6f7102cc7247b204931312e034a2C3D5C2432866"
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("ZunXBT");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
+    0
+  );
+  await safeMintTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
